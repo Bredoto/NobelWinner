cb8b17bbf4ce8b448c12b867d57ed61fa686e392
Bredoto <serg.rumyantsev@gmail.com>
2021-02-10 13:28:51 +0200

    class added

--- Winner.py
+++ Winner.py
@@ -6,8 +6,74 @@ def get_category(self):
     return self.category
 
 
+def get_prize(self):
+    return self.prize
+
+
+def get_motivation(self):
+    return self.motivation
+
+
+def get_prizeshare(self):
+    return self.prizeshare
+
+
+def get_laureateid(self):
+    return self.laureateid
+
+
+def get_laureatetype(self):
+    return self.laureatetype
+
+
+def get_fullname(self):
+    return self.fullname
+
+
+def get_birthdate(self):
+    return self.birthdate
+
+
+def get_birthcity(self):
+    return self.birthcity
+
+
+def get_birthcountry(self):
+    return self.birthcountry
+
+
+def get_sex(self):
+    return self.sex
+
+
+def get_organizationname(self):
+    return self.organizationname
+
+
+def get_organizationcity(self):
+    return self.organizationcity
+
+
+def get_organizationcountry(self):
+    return self.organizationcountry
+
+
+def get_deathdate(self):
+    return self.deathdate
+
+
+def get_deathcity(self):
+    return self.deathcity
+
+
+def get_deathcountry(self):
+    return self.deathcountry
+
+
 class Winner:
-    def __init__(self, year, category, prize, motivation, prizeshare, laureateid, laureatetype, fullname):
+    def __init__(self, year, category, prize, motivation, prizeshare, laureateid, laureatetype, fullname, birthdate,
+                 birthcity, birthcountry, sex, organizationname, organizationcity, organizationcountry, deathdate,
+                 deathcity, deathcountry):
         self.year = year
         self.category = category
         self.prize = prize
@@ -16,5 +82,13 @@ class Winner:
         self.laureateid = laureateid
         self.laureatetype = laureatetype
         self.fullname = fullname
-
-
+        self.birthdate = birthdate
+        self.birthcity = birthcity
+        self.birthcountry = birthcountry
+        self.sex = sex
+        self.organizationname = organizationname
+        self.organizationcity = organizationcity
+        self.organizationcountry = organizationcountry
+        self.deathdate = deathdate
+        self.deathcity = deathcity
+        self.deathcountry = deathcountry
--- nobel.py
+++ nobel.py
@@ -1,9 +1,5 @@
-from random import random, sample
-from random import seed
-from random import randint
-from random import choice
 import csv
-
+from Winner import Winner
 
 def main():
     with open('archive.csv', newline='') as f:
@@ -11,6 +7,11 @@ def main():
         reader = csv.reader(f)
         for row in reader:
             print(row)
+
+a = Winner('2016', 'Physics', 'The Nobel Prize in Physics 2016', '"for theoretical discoveries of topological phase transitions and topological phases of matter"', '1/4', '930', 'Individual', 'J. Michael Kosterlitz', '1943-06-22', 'Aberdeen', 'United Kingdom', 'Male', 'Brown University', 'Providence, RI', 'United States of America', '', '', '')
+
+
+
 
 
 if __name__ == "__main__":
