d42c13a91340ded6d5fcddead124e67775d650a7
Bredoto <serg.rumyantsev@gmail.com>
2021-02-11 22:05:27 +0200

    sql plus

--- nobel.py
+++ nobel.py
@@ -2,7 +2,7 @@ import csv
 from Winner import Winner
 import mysql.connector
 from mysql.connector import Error
-
+# Simple python tool to create SQL INSERT statements from CSV files.  https://github.com/najiji/csv2sql
 
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
@@ -29,9 +29,20 @@ def create_database(connection, query):
 
 
 def main():
-    connection = create_connection("localhost", "root", "qazxsw", "NobelWinners")
+    try:
+        connection = create_connection("localhost", "root", "qazxswED4$", "NobelWinners")
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("select database ();")
+            record = cursor.fetchone()
+            print("Connected to database:", record)
+            cursor.execute("CREATE TABLE winners (Year FLOAT(2,1) NOT NULL,Category CHAR(11) NOT NULL,Prize CHAR(11) NOT NULL,Motivation CHAR(11) NOT NULL,PrizeShare  CHAR(11) NOT NULL,LaureateID CHAR(11) NOT NULL,LaureateType CHAR(11) NOT NULL,FullName CHAR(11) NOT NULL,BirthDate CHAR(11) NOT NULL,BirthCity  CHAR(11) NOT NULL,BirthCountry CHAR(11) NOT NULL,Sex CHAR(11) NOT NULL,OrganizationName CHAR(11) NOT NULL,OrganizationCity CHAR(11) NOT NULL,OrganizationCountry CHAR(11) NOT NULL,DeathDate CHAR(11) NOT NULL,DeathCity  CHAR(11) NOT NULL,DeathCountry CHAR(11) NOT NULL)")
+            print("winners table is created....")
+    except Error as e:
 
-    with open('archive.csv', newline='') as f:
+
+
+    #with open('archive.csv', newline='') as f:
         # with open('data.csv', newline='') as f:
         reader = csv.reader(f)
         # for row in reader:
