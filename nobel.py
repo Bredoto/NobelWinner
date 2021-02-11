2a223129ec2171074408726ccb7dcc8856b80b47
Bredoto <serg.rumyantsev@gmail.com>
2021-02-11 23:19:02 +0200

    record

--- nobel.py
+++ nobel.py
@@ -2,6 +2,8 @@ import csv
 from Winner import Winner
 import mysql.connector
 from mysql.connector import Error
+
+
 # Simple python tool to create SQL INSERT statements from CSV files.  https://github.com/najiji/csv2sql
 
 def create_connection(host_name, user_name, user_password, db_name):
@@ -30,26 +32,27 @@ def create_database(connection, query):
 
 def main():
     try:
+        # with open('archive.csv', newline='') as f:
+        f = open('archive.csv', newline='')
+        reader = csv.reader(f)
+
         connection = create_connection("localhost", "root", "qazxswED4$", "NobelWinners")
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("select database ();")
             record = cursor.fetchone()
             print("Connected to database:", record)
-            cursor.execute("CREATE TABLE winners (Year FLOAT(2,1) NOT NULL,Category CHAR(11) NOT NULL,Prize CHAR(11) NOT NULL,Motivation CHAR(11) NOT NULL,PrizeShare  CHAR(11) NOT NULL,LaureateID CHAR(11) NOT NULL,LaureateType CHAR(11) NOT NULL,FullName CHAR(11) NOT NULL,BirthDate CHAR(11) NOT NULL,BirthCity  CHAR(11) NOT NULL,BirthCountry CHAR(11) NOT NULL,Sex CHAR(11) NOT NULL,OrganizationName CHAR(11) NOT NULL,OrganizationCity CHAR(11) NOT NULL,OrganizationCountry CHAR(11) NOT NULL,DeathDate CHAR(11) NOT NULL,DeathCity  CHAR(11) NOT NULL,DeathCountry CHAR(11) NOT NULL)")
+            cursor.execute(
+                "CREATE TABLE winners (Year CHAR(11) NOT NULL,Category CHAR(11) NOT NULL,Prize CHAR(254) NOT NULL,Motivation CHAR(254) NOT NULL,PrizeShare  CHAR(254) NOT NULL,LaureateID CHAR(11) NOT NULL,LaureateType CHAR(48) NOT NULL,FullName CHAR(48) NOT NULL,BirthDate CHAR(48) NOT NULL,BirthCity  CHAR(11) NOT NULL,BirthCountry CHAR(48) NOT NULL,Sex CHAR(11) NOT NULL,OrganizationName CHAR(48) NOT NULL,OrganizationCity CHAR(48) NOT NULL,OrganizationCountry CHAR(48) NOT NULL,DeathDate CHAR(48) NOT NULL,DeathCity  CHAR(48) NOT NULL,DeathCountry CHAR(48) NOT NULL)")
             print("winners table is created....")
+            for row in reader:
+                sql = """INSERT INTO NobelWinners.winners VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+                cursor.execute(sql, row)  # cursor.execute(sql, tuple(row))
+                print("Record inserted")
+                connection.commit()
+        f.close()
     except Error as e:
-
-
-
-    #with open('archive.csv', newline='') as f:
-        # with open('data.csv', newline='') as f:
-        reader = csv.reader(f)
-        # for row in reader:
-        # print(row)
-
-
-# a = Winner('2016', 'Physics', 'The Nobel Prize in Physics 2016', '"for theoretical discoveries of topological phase transitions and topological phases of matter"', '1/4', '930', 'Individual', 'J. Michael Kosterlitz', '1943-06-22', 'Aberdeen', 'United Kingdom', 'Male', 'Brown University', 'Providence, RI', 'United States of America', '', '', '')
+        print("Error while connecting to MySQL", e)
 
 
 if __name__ == "__main__":
