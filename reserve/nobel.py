563910e39a28b369d53128d9a6c9eb9415c81698
Bredoto <serg.rumyantsev@gmail.com>
2021-02-12 14:47:45 +0200

    sql plus2

--- reserve/nobel.py
+++ reserve/nobel.py
@@ -13,9 +13,7 @@ def create_connection(host_name, user_name, user_password, db_name):
             host=host_name,
             user=user_name,
             passwd=user_password,
-            database=db_name,
-            use_unicode=True,
-            charset="utf8"
+            database=db_name
         )
         print("Connection to MySQL DB successful")
     except Error as e:
@@ -44,15 +42,11 @@ def main():
             cursor.execute("select database ();")
             record = cursor.fetchone()
             print("Connected to database:", record)
-            cursor.execute("SET NAMES utf8mb4;")  # or utf8 or any other charset you want to handle
-            cursor.execute("SET CHARACTER SET utf8mb4;")  # same as above
-            cursor.execute("SET character_set_connection=utf8mb4;")  # same as above
-
             cursor.execute(
-                "CREATE TABLE winners (Year CHAR(11) NOT NULL,Category CHAR(11) NOT NULL,Prize CHAR(254) NOT NULL,Motivation CHAR(254) NOT NULL,PrizeShare  CHAR(254) NOT NULL,LaureateID CHAR(11) NOT NULL,LaureateType CHAR(48) NOT NULL,FullName CHAR(48) NOT NULL,BirthDate CHAR(48) NOT NULL,BirthCity  CHAR(48) NOT NULL,BirthCountry CHAR(48) NOT NULL,Sex CHAR(11) NOT NULL,OrganizationName CHAR(48) NOT NULL,OrganizationCity CHAR(48) NOT NULL,OrganizationCountry CHAR(48) NOT NULL,DeathDate CHAR(48) NOT NULL,DeathCity  CHAR(48) NOT NULL,DeathCountry CHAR(48) NOT NULL)")
+                "CREATE TABLE winners (Year CHAR(11) NOT NULL,Category CHAR(11) NOT NULL,Prize CHAR(254) NOT NULL,Motivation CHAR(254) NOT NULL,PrizeShare  CHAR(254) NOT NULL,LaureateID CHAR(11) NOT NULL,LaureateType CHAR(48) NOT NULL,FullName CHAR(48) NOT NULL,BirthDate CHAR(48) NOT NULL,BirthCity  CHAR(11) NOT NULL,BirthCountry CHAR(48) NOT NULL,Sex CHAR(11) NOT NULL,OrganizationName CHAR(48) NOT NULL,OrganizationCity CHAR(48) NOT NULL,OrganizationCountry CHAR(48) NOT NULL,DeathDate CHAR(48) NOT NULL,DeathCity  CHAR(48) NOT NULL,DeathCountry CHAR(48) NOT NULL)")
             print("winners table is created....")
             for row in reader:
-                sql = u"""INSERT INTO NobelWinners.winners VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+                sql = """INSERT INTO NobelWinners.winners VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
                 cursor.execute(sql, row)  # cursor.execute(sql, tuple(row))
                 print("Record inserted")
                 connection.commit()
